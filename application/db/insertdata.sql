2636381d9a1ede4e4c7043fde6b3839af3a66577
Yegor Maksymchuk <ymaksymc@redhat.com>
2019-02-09 19:51:52 +0200

    add docker

--- application/db/insertdata.sql
+++ application/db/insertdata.sql
@@ -1,14 +1,3 @@
-DROP SCHEMA IF EXISTS sbt_demo;
-CREATE SCHEMA IF NOT EXISTS sbt_demo DEFAULT CHARACTER SET utf8;
-
-DROP TABLE sbt_demo.films;
-
-CREATE TABLE sbt_demo.films(
-   id           BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name         VARCHAR(100) NOT NULL,
-  director         VARCHAR(100) NOT NULL
-);
-
 insert into sbt_demo.films (name, director)
 values
       ('The Lord of the Rings: The Fellowship of the Ring','Peter_Jackson'),
@@ -17,5 +6,3 @@ values
       ('The Hobbit: An Unexpected Journey','Peter_Jackson'),
       ('The Hobbit: The Desolation of Smaug','Peter_Jackson'),
       ('The Hobbit: The Battle of the Five Armies','Peter_Jackson');
-
-SELECT * FROM sbt_demo.films;
